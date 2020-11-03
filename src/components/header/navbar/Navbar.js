235896f19d9f346efb6750a5d555fa3efc27eb0d
Bastien750 <56043382+Bastien750@users.noreply.github.com>
2020-11-03 22:59:14 +0100

    Fix some front end bug on phone

--- src/components/header/navbar/Navbar.js
+++ src/components/header/navbar/Navbar.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { HashLink as HLink } from "react-router-hash-link";
+import { NavHashLink as HLink } from "react-router-hash-link";
 import { SidebarData } from "./SidebarData";
 import "./Navbar.css";
+import { useMediaQuery } from "react-responsive";
 
 function Navbar() {
-  const [sidebar, setSidebar] = useState(true);
-
+  const isMobile = useMediaQuery({ query: "(max-width: 1100px)" });
+  const [sidebar, setSidebar] = useState(isMobile ? false : true);
   function showSidebar() {
     return setSidebar(!sidebar);
   }
@@ -30,7 +31,15 @@ function Navbar() {
           {SidebarData.map((item, index) => {
             return (
               <li key={index} className={item.cName}>
-                <HLink to={item.path}>
+                <HLink
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={1000}
+                  to={item.path}
+                  activeClassName="selected"
+                >
                   {item.icon}
                   <span>{item.title}</span>
                 </HLink>
