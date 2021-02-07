08baccb99d436539b17843aba7bb40dd296b0af1
Bastien750 <56043382+Bastien750@users.noreply.github.com>
2021-02-07 16:58:37 +0100

    The logo is a link toà the home page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "./navbar/Navbar";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <div className="header">
         <Navbar />
-        <h2>C3F</h2>
+        <Link to="/" className="home-link">
+          <h2>C3F</h2>
+        </Link>
       </div>
     </header>
   );
